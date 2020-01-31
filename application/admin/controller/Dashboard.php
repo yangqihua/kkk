6b1071d5d7489b357e5e656c2a2b387dfaf4d1c1
yangqihua <904693433@qq.com>
2020-02-01 01:33:12 +0800

    chart

--- application/admin/controller/Dashboard.php
+++ application/admin/controller/Dashboard.php
@@ -5,6 +5,7 @@ namespace app\admin\controller;
 use app\common\controller\Backend;
 use fast\Http;
 use think\Config;
+use think\Db;
 
 /**
  * 控制台
@@ -18,7 +19,7 @@ class Dashboard extends Backend
     /**
      * 查看
      */
-    public function index()
+    public function test()
     {
         $size = input('size',1000);
         $period = input('period','15min');;
@@ -50,6 +51,66 @@ class Dashboard extends Backend
 
         $this->view->assign([
             'lineData' => $lineData
+        ]);
+        return $this->view->fetch();
+    }
+
+    public function index()
+    {
+
+        $legend = ['MONEY','USDT','ETH','BCH','BTC'];
+        $series = [];
+        $date = [];
+        $firstDay = strtotime(date('Y-m-d', strtotime("-6 month")));
+        $sql = 'SELECT from_unixtime(createtime,\'%Y-%m-%d %H:%i\') createTime,content from jun_balance 
+WHERE createtime>:firstDay and username="yang";';
+        $yangBalances = Db::query($sql, ['firstDay' => $firstDay]);
+        $moneyData = [];
+        foreach ($yangBalances as $key=>$balance){
+            $content = json_decode($balance['content'],true);
+            foreach ($legend as $k=>$v){
+                $moneyData[$v][]=$content[$v];
+            }
+            $date[] = $balance['createTime'];
+        }
+        foreach ($legend as $k=>$v){
+            $series[] = [
+                'name' => $v,
+                'type' => 'line',
+                'data' => $moneyData[$v]
+            ];
+        }
+        $lineData = ['legend' => $legend, 'date' => $date, 'series' => $series];
+
+        $legend = ['MONEY','USDT','EOS','BCH','BTC'];
+        $series = [];
+        $date = [];
+        $firstDay = strtotime(date('Y-m-d', strtotime("-6 month")));
+        $sql = 'SELECT from_unixtime(createtime,\'%Y-%m-%d %H:%i\') createTime,content from jun_balance 
+WHERE createtime>:firstDay and username="xu";';
+        $yangBalances = Db::query($sql, ['firstDay' => $firstDay]);
+        $moneyData = [];
+        foreach ($yangBalances as $key=>$balance){
+            $content = json_decode($balance['content'],true);
+            foreach ($legend as $k=>$v){
+                $moneyData[$v][]=$content[$v];
+            }
+            $date[] = $balance['createTime'];
+        }
+        foreach ($legend as $k=>$v){
+            $series[] = [
+                'name' => $v,
+                'type' => 'line',
+                'data' => $moneyData[$v]
+            ];
+        }
+        $xuLineData = ['legend' => $legend, 'date' => $date, 'series' => $series];
+
+
+
+        $this->view->assign([
+            'lineData' => $lineData,
+            'xuLineData' => $xuLineData,
         ]);
         return $this->view->fetch();
     }
--- public/assets/js/backend/dashboard.js
+++ public/assets/js/backend/dashboard.js
@@ -6,7 +6,7 @@ define(['jquery', 'bootstrap', 'backend', 'addtabs', 'table', 'echarts', 'echart
             var inventoryChart = Echarts.init(document.getElementById('k_chart'));
             var inventoryOption = {
                 title: {
-                    text: 'K线对比',
+                    text: '余额趋势',
                     textStyle: {
                         color: '#27C24C',
                         fontSize: '16'
@@ -63,6 +63,67 @@ define(['jquery', 'bootstrap', 'backend', 'addtabs', 'table', 'echarts', 'echart
                 series: lineData['series']
             };
             inventoryChart.setOption(inventoryOption);
+
+            var xuInventoryChart = Echarts.init(document.getElementById('xu_chart'));
+            var xuInventoryOption = {
+                title: {
+                    text: '余额趋势',
+                    textStyle: {
+                        color: '#27C24C',
+                        fontSize: '16'
+                    },
+                },
+                tooltip: {
+                    trigger: 'axis'
+                },
+                legend: {
+                    data: xuLineData['legend']
+                },
+                grid: {
+                    "bottom": 100,
+                },
+                toolbox: {
+                    feature: {
+                        saveAsImage: {}
+                    }
+                },
+                xAxis: {
+                    type: 'category',
+                    boundaryGap: false,
+                    data: xuLineData['date']
+                },
+                yAxis: {
+                    type: 'value'
+                },
+                dataZoom: [
+                    {
+                        "xAxisIndex": [
+                            0
+                        ],
+                        bottom: 30,
+                        "start": 0,
+                        "end": 100,
+                        handleIcon: 'path://M306.1,413c0,2.2-1.8,4-4,4h-59.8c-2.2,0-4-1.8-4-4V200.8c0-2.2,1.8-4,4-4h59.8c2.2,0,4,1.8,4,4V413z',
+                        handleSize: '110%',
+                        handleStyle: {
+                            color: "#aaa",
+                        },
+                        textStyle: {
+                            color: "#27C24C"
+                        },
+                        borderColor: "#aaa"
+                    },
+                    {
+                        "type": "inside",
+                        "show": true,
+                        // "height": 15,
+                        "start": 30,
+                        "end": 100
+                    }
+                ],
+                series: xuLineData['series']
+            };
+            xuInventoryChart.setOption(xuInventoryOption);
         }
     };
 
