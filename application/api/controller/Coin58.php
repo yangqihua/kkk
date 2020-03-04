7a20ee211fa54f823cc5c9bfda22920e3f1395c1
yangqihua <904693433@qq.com>
2020-03-04 18:59:55 +0800

    3倍

--- application/api/controller/Coin58.php
+++ application/api/controller/Coin58.php
@@ -23,7 +23,7 @@ class Coin58 extends Api
     private $secret = '';
     private $access_token = '';
 
-    private $moneyRate = 5;
+    private $moneyRate = 3;
 
     // 1.行情orderbook
     // 2.账户余额
