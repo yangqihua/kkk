5593dd3270c57289a92850c82b8ee3009fcc553a
yangqihua <904693433@qq.com>
2021-02-22 17:25:37 +0800

    策略统计

--- application/admin/controller/Dashboard.php
+++ application/admin/controller/Dashboard.php
@@ -19,71 +19,70 @@ class Dashboard extends Backend
     protected $noNeedLogin = ['*'];
     protected $noNeedRight = ['*'];
 
-    /**
-     * 查看
-     */
-    public function test()
-    {
-        $size = input('size',1000);
-        $period = input('period','15min');;
-
-        $result = [];
-        $kinds = [
-            '火币现货'=>"https://api.huobi.pro/market/history/kline?period=".$period."&size=".$size."&symbol=btcusdt",
-            '火币期货' => "https://api.hbdm.com/market/history/kline?period=" . $period . "&size=" . $size . "&symbol=BTC_CQ",
-//            'OK现货'=>"",
-//            'OK期货'=>"",
-        ];
-
-        foreach ($kinds as $key => $item) {
-            $res = json_decode(Http::get($item),true);
-            $result[$key] = $res?$res['data']:[];
-        }
-
-        $legend = array_keys($kinds);
-        $date = array_keys($result['火币期货']);
-        $series = [];
-        foreach ($result as $key => $itemData) {
-            $series[] = [
-                'name' => $key,
-                'type' => 'line',
-                'data' => array_column($itemData,'close')
-            ];
-        }
-        $lineData = ['legend' => $legend, 'date' => $date, 'series' => $series];
-
-        $this->view->assign([
-            'lineData' => $lineData
-        ]);
-        return $this->view->fetch();
-    }
-
     public function index()
     {
+        $chartList = [];
+        $markets = ['doge_usdt'];
+        foreach ($markets as $k => $market) {
+            $sql = 'SELECT * from jun_history WHERE market=:market order by id asc limit 1;';
+            $histories = Db::query($sql, ['market' => $market]);
+            $initMoney = $histories[0]['money_before'];
+            $initCoin = $histories[0]['coin_before'];
 
-        $legend = ['MONEY','USDT','XTZ3L','BSV3L'];
-        $series = [];
-        $date = [];
-        $firstDay = strtotime(date('Y-m-d', strtotime("-6 month")));
-        $sql = 'SELECT from_unixtime(createtime,\'%Y-%m-%d %H:%i\') createTime,content from jun_balance 
-WHERE id>8973 and createtime>:firstDay and username="yang";';
-        $yangBalances = Db::query($sql, ['firstDay' => $firstDay]);
-        $moneyData = [];
-        foreach ($yangBalances as $key=>$balance){
-            $content = json_decode($balance['content'],true);
-            foreach ($legend as $k=>$v){
-                $moneyData[$v][]=$content[$v];
+            $firstDay = strtotime(date('Y-m-d', strtotime("-3 month")));
+            $sql = 'SELECT from_unixtime(createtime,\'%Y-%m-%d %H:%i\') createTime,price,cap_after from jun_history 
+WHERE createtime>:firstDay and market=:market;';
+            $histories = Db::query($sql, ['firstDay' => $firstDay, 'market' => $market]);
+            $initCap = [];
+            $quantCap = [];
+            $date = [];
+            $series = [];
+            $liData = [];
+            foreach ($histories as $key => $history) {
+                $initCap[] = $initMoney + $initCoin * $history['price'];
+                $quantCap[] = $history['cap_after'];
+                $date = $history['createTime'];
             }
-            $date[] = $balance['createTime'];
+            $liData['原市值'] = $initCap;
+            $liData['网格市值'] = $quantCap;
+            $legend = ['原市值', '网格市值'];
+            foreach ($legend as $tKey => $tValue) {
+                $series[] = [
+                    'name' => $tValue,
+                    'type' => 'line',
+                    'data' => $liData[$tValue]
+                ];
+            }
+            $chartList[$market] = ['legend' => $legend, 'date' => $date, 'series' => $series];
         }
-        foreach ($legend as $k=>$v){
-            $series[] = [
-                'name' => $v,
-                'type' => 'line',
-                'data' => $moneyData[$v]
-            ];
-        }
-        $lineData = ['legend' => $legend, 'date' => $date, 'series' => $series];
+        $this->view->assign([
+            'chartList' => $chartList,
+        ]);
+        return $this->view->fetch();
+
+//        $legend = ['MONEY', 'USDT', 'XTZ3L', 'BSV3L'];
+//        $series = [];
+//        $date = [];
+//        $firstDay = strtotime(date('Y-m-d', strtotime("-6 month")));
+//        $sql = 'SELECT from_unixtime(createtime,\'%Y-%m-%d %H:%i\') createTime,content from jun_balance
+//WHERE id>8973 and createtime>:firstDay and username="yang";';
+//        $yangBalances = Db::query($sql, ['firstDay' => $firstDay]);
+//        $moneyData = [];
+//        foreach ($yangBalances as $key => $balance) {
+//            $content = json_decode($balance['content'], true);
+//            foreach ($legend as $k => $v) {
+//                $moneyData[$v][] = $content[$v];
+//            }
+//            $date[] = $balance['createTime'];
+//        }
+//        foreach ($legend as $k => $v) {
+//            $series[] = [
+//                'name' => $v,
+//                'type' => 'line',
+//                'data' => $moneyData[$v]
+//            ];
+//        }
+//        $lineData = ['legend' => $legend, 'date' => $date, 'series' => $series];
 
 //        $legend = ['MONEY','USDT','EOS','BCH','BTC'];
 //        $series = [];
@@ -107,13 +106,52 @@ WHERE id>8973 and createtime>:firstDay and username="yang";';
 //                'data' => $moneyData[$v]
 //            ];
 //        }
-        $xuLineData = ['legend' => $legend, 'date' => [], 'series' => $series];
+//        $xuLineData = ['legend' => $legend, 'date' => [], 'series' => $series];
+//
+//
+//        $this->view->assign([
+//            'lineData' => $lineData,
+//            'xuLineData' => $xuLineData,
+//        ]);
+//        return $this->view->fetch();
+    }
 
 
+    /**
+     * 查看
+     */
+    public function test()
+    {
+        $size = input('size', 1000);
+        $period = input('period', '15min');;
+
+        $result = [];
+        $kinds = [
+            '火币现货' => "https://api.huobi.pro/market/history/kline?period=" . $period . "&size=" . $size . "&symbol=btcusdt",
+            '火币期货' => "https://api.hbdm.com/market/history/kline?period=" . $period . "&size=" . $size . "&symbol=BTC_CQ",
+//            'OK现货'=>"",
+//            'OK期货'=>"",
+        ];
+
+        foreach ($kinds as $key => $item) {
+            $res = json_decode(Http::get($item), true);
+            $result[$key] = $res ? $res['data'] : [];
+        }
+
+        $legend = array_keys($kinds);
+        $date = array_keys($result['火币期货']);
+        $series = [];
+        foreach ($result as $key => $itemData) {
+            $series[] = [
+                'name' => $key,
+                'type' => 'line',
+                'data' => array_column($itemData, 'close')
+            ];
+        }
+        $lineData = ['legend' => $legend, 'date' => $date, 'series' => $series];
 
         $this->view->assign([
-            'lineData' => $lineData,
-            'xuLineData' => $xuLineData,
+            'lineData' => $lineData
         ]);
         return $this->view->fetch();
     }
--- public/assets/js/backend/dashboard.js
+++ public/assets/js/backend/dashboard.js
@@ -3,10 +3,11 @@ define(['jquery', 'bootstrap', 'backend', 'addtabs', 'table', 'echarts', 'echart
     var Controller = {
         index: function () {
             // 基于准备好的dom，初始化echarts实例
-            var inventoryChart = Echarts.init(document.getElementById('k_chart'));
+            var lineData = chartList['doge_usdt'];
+            var inventoryChart = Echarts.init(document.getElementById('doge_chart'));
             var inventoryOption = {
                 title: {
-                    text: '余额趋势',
+                    text: 'doge策略统计',
                     textStyle: {
                         color: '#27C24C',
                         fontSize: '16'
@@ -29,7 +30,8 @@ define(['jquery', 'bootstrap', 'backend', 'addtabs', 'table', 'echarts', 'echart
                 xAxis: {
                     type: 'category',
                     boundaryGap: false,
-                    data: lineData['date']
+                    // data: lineData['date']
+                    data: []
                 },
                 yAxis: {
                     type: 'value',
@@ -65,69 +67,6 @@ define(['jquery', 'bootstrap', 'backend', 'addtabs', 'table', 'echarts', 'echart
                 series: lineData['series']
             };
             inventoryChart.setOption(inventoryOption);
-
-            var xuInventoryChart = Echarts.init(document.getElementById('xu_chart'));
-            var xuInventoryOption = {
-                title: {
-                    text: '余额趋势',
-                    textStyle: {
-                        color: '#27C24C',
-                        fontSize: '16'
-                    },
-                },
-                tooltip: {
-                    trigger: 'axis'
-                },
-                legend: {
-                    data: xuLineData['legend']
-                },
-                grid: {
-                    "bottom": 100,
-                },
-                toolbox: {
-                    feature: {
-                        saveAsImage: {}
-                    }
-                },
-                xAxis: {
-                    type: 'category',
-                    boundaryGap: false,
-                    data: xuLineData['date']
-                },
-                yAxis: {
-                    type: 'value',
-                    boundaryGap: [0, '100%'],
-                    scale: true,
-                },
-                dataZoom: [
-                    {
-                        "xAxisIndex": [
-                            0
-                        ],
-                        bottom: 30,
-                        "start": 0,
-                        "end": 100,
-                        handleIcon: 'path://M306.1,413c0,2.2-1.8,4-4,4h-59.8c-2.2,0-4-1.8-4-4V200.8c0-2.2,1.8-4,4-4h59.8c2.2,0,4,1.8,4,4V413z',
-                        handleSize: '110%',
-                        handleStyle: {
-                            color: "#aaa",
-                        },
-                        textStyle: {
-                            color: "#27C24C"
-                        },
-                        borderColor: "#aaa"
-                    },
-                    {
-                        "type": "inside",
-                        "show": true,
-                        // "height": 15,
-                        "start": 30,
-                        "end": 100
-                    }
-                ],
-                series: xuLineData['series']
-            };
-            xuInventoryChart.setOption(xuInventoryOption);
         }
     };
 
